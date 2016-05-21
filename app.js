2bb678b632f0dcbc680c1aa942f29e46bb5b3f15
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-21 08:12:16 -0400

    adds styling

--- app.js
+++ app.js
@@ -3,18 +3,16 @@
 (function(){
   angular
   .module("todo", [])
-  .controller("todos_controller", ControllerFunction);
+  .controller("todoController", ControllerFunction);
 
   function ControllerFunction(){
     var vm = this;
-    vm.todos = [
-      "walk the dog",
-      "debug angular",
-      "feed the fish"
-    ];
+    vm.todos = ["learn angular"];
+
     vm.destroy = function(todos_index){
       vm.todos.splice(todos_index,1);
     }
+    
     vm.create = function(){
       vm.todos.push(angular.copy(vm.new_todo));
       vm.new_todo = "";
